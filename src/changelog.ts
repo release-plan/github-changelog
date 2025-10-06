5fca426ad7706f5e7a6841215c15cebf61003ed8
Patrick Pircher <patricklx@users.noreply.github.com>
2025-10-06 10:04:59 +0200

    fix

--- src/changelog.ts
+++ src/changelog.ts
@@ -215,7 +215,7 @@ export default class Changelog {
         }
 
         let prUserLogin = commit.githubIssue?.user.login;
-        if (prUserLogin && (!this.ignoreCommitter(prUserLogin) || !commit.githubIssue.labels.includes(this.config.wildcardLabel))) {
+        if (prUserLogin && (!this.ignoreCommitter(prUserLogin) || !commit.githubIssue?.labels.map(l => l.name).includes(this.config.wildcardLabel))) {
           releaseMap[currentTag].commits.push(commit);
         }
       }
